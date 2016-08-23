c7d2201787efb7e5d1506fd42da4d56a4e2e37c9
xielc <13147163660@163.com>
2016-08-23 10:37:06 +0800

    xielc

---
+++ src/com/Enum/HyEnum.java
@@ -0,0 +1,32 @@
+package com.Enum;
+
+public enum HyEnum {
+	
+	jrl("金融类") {},
+	kjl("科技类") {},
+	qcl("汽车类") {},
+	kxl("快消类") {},
+	fdcl("房地产类") {},
+	yxl("游戏类") {},
+	shfwl("生活服务类") {},
+	yyl("医药类") {},
+	swfwl("商务服务类") {},
+	jtl("交通类") {},
+	lyl("旅游类") {},
+	jyl("教育类") {},
+	zfjgl("政府机构类") {},
+	mtl("媒体类") {},
+	qtjgl("其他机构类") {};
+
+	private String value;
+
+	private HyEnum(String value)
+	{
+		this.value = value;
+	}
+
+	public String getValue()
+	{
+		return value;
+	}
+}
